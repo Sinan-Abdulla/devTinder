f92c789e6b894ebcd12a6a0f540541b9de3688cf
sinan abdulla <sinanabdulla822@gmail.com>
2025-03-18 17:23:44 +0530

    Fixed user retrieval bug

--- src/app.js
+++ src/app.js
@@ -2,21 +2,41 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
+const user = require("./models/user");
 
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-    console.log(req.body);
+});
 
-    const user = new User(req.body);
 
+
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.email;
     try {
-        await user.save();
-        res.send("user adedd succesfully!!");
-    }catch{
-        res.status(400).send("error saving the datas:" +erro.message);
+        console.log("haii");
+
+        const user = await User.find({});
+        if (!user) {
+            res.status(404).send({ message: "User not found" });
+        } else {
+            res.send(user);
+        }
+
+        /*
+        const users = await user.find({email: userEmail });
+        if (users.length === 0) {
+            res.status(404).send("User not found");
+        } else {
+            res.send(users);
+        }*/
+
+
+
+
+    } catch (err) {
+        res.status(400).send("Error");
     }
-   
 });
 
 
