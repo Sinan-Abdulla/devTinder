447775ed8865f934891159ad5a8f80e7ee78f178
sinan abdulla <sinanabdulla822@gmail.com>
2025-06-14 23:41:29 +0530

    l

--- src/utiles/socket.js
+++ src/utiles/socket.js
@@ -1,4 +1,5 @@
 const { Server } = require("socket.io");
+const { Chat } = require('../models/chat');
 
 const initializeSocket = (server) => {
     const io = new Server(server, {
@@ -17,10 +18,39 @@ const initializeSocket = (server) => {
             console.log(`${firstName} joined room: ${roomId}`);
         });
 
-        socket.on("sendmessage", (message) => {
-            const roomId = getRoomId(message.userId, message.targetUserId);
-            socket.to(roomId).emit("receivemessage", message);
+        socket.on("sendmessage", async (message) => {
+
+            const { userId, targetUserId, content } = message;
+
+            try {
+                const roomId = getRoomId(message.userId, message.targetUserId);
+                let chat = await Chat.findOne({
+                    participants: { $all: [userId, targetUserId] }
+                });
+
+                if (!chat) {
+                    chat = new Chat({
+                        participants: [userId, targetUserId],
+                        messages: [],
+                    });
+                }
+                chat.messages.push({
+                    senderId: userId,
+                    text: content,
+                });
+
+                await chat.save();
+
+                socket.to(roomId).emit("receivemessage", message);
+
+            } catch (err) {
+                console.log(err);
+            }
+
+
         });
+
+
 
         socket.on("disconnect", () => {
             console.log("User disconnected:", socket.id);
